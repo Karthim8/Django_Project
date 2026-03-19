73d72e856c4f34d4c318e5e8806e8d67d1c6a1c7
Karthim8 <karthikeyanspro@gmail.com>
2026-03-19 12:15:44 +0530

    hh

---
+++ appname/consumers.py
@@ -0,0 +1,30 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class ChatConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        self.room_name = "global_chat"
+
+        await self.channel_layer.group_add(
+            self.room_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+
+        await self.channel_layer.group_send(
+            self.room_name,
+            {
+                'type': 'chat_message',
+                'message': data['message']
+            }
+        )
+
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({
+            'message': event['message']
+        }))
---
+++ project1/asgi.py
@@ -0,0 +1,31 @@
+"""
+ASGI config for project1 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1.settings')
+
+# application = get_asgi_application()
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from appname.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+
+    # 🔥 THIS PART HANDLES WEBSOCKET
+    "websocket": URLRouter(
+        websocket_urlpatterns
+    ),
+})
